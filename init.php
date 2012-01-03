c2eca7a5a66d62214cb2417dacbc2b6560d6aae7
oskob <oskar@oskarart.net>
2012-01-03 11:02:32 -0600

    lol

---
+++ init.php
@@ -0,0 +1,30 @@
+<?php
+
+session_start();
+require "settings.php";
+
+if(!@mysql_connect($settings['dbhost'], $settings['dbuser'], $settings['dbpass']))
+{
+	echo "Could not connect to database";
+	exit;
+}
+
+if(!@mysql_select_db($settings['dbdb'])) // first time, setup
+{
+	$sql = "CREATE DATABASE IF NOT EXISTS " . $settings['dbdb'] . ";";
+	mysql_query($sql) or die(mysql_error());
+	mysql_select_db($settings['dbdb']);	
+
+	$sql = "CREATE TABLE mail (
+		id INT NOT NULL AUTO_INCREMENT,
+		subject VARCHAR(255),
+		message TEXT,
+		`date` DATETIME,
+		`to` VARCHAR(100),
+		`from` VARCHAR(100),
+		PRIMARY KEY (id)
+		)";
+	
+	mysql_query($sql) or dir(mysql_error());
+
+}

d05074d4420373e1409ad2c2dc222914fd22bdb4
oskob <oskar@oskarart.net>
2012-01-03 07:22:31 -0600

    lol

---
+++ admin.php
@@ -0,0 +1,16 @@
+<?php
+
+require "settings.php"
+
+?>
+<html>
+	<head>
+		<title>Super Advanced Power Admin Tool</title>
+	</head>
+	<body>
+		<h1>Super Advanced Power Admin Tool</h2>
+
+
+
+	</body>
+</html>
